be90c74c7ed75568793e64d8a30e612bc411a680
marcelburger <burger.mr@gmail.com>
2018-11-29 18:24:28 +0100

    Perform FizzBuzz algo on 10 instead of 7

---
+++ FizzBuzz.py
@@ -0,0 +1,17 @@
+def fizz_buzz(input):
+    if input % 3 == 0:
+        if input % 5 == 0:
+            return "FizzBuzz"
+        else:
+            return "Fizz"
+    elif input % 5 == 0:
+        return "Buzz"
+    else:
+        return input
+
+
+
+
+print(fizz_buzz(10))
+
+
